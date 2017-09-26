8899b0d13a6a488e5277e53c69b15b95ef8b0037
Ronak J. Gandhi <rjg184@top.cs.rutgers.edu>
2017-09-26 16:26:49 -0400

    finished tokenizer; trying to fill in array (segfaults/problems)

--- Sorter/sorter.c
+++ Sorter/sorter.c
@@ -25,64 +25,101 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "Sorter.h"
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	
 	//char buffer[1000];
 	char *buffer = malloc(1000);
-	fgets(buffer,1000,stdin);
-	
-	/*while(fgets(&buffer, 1000, stdin)){
-		
-		printf("%s", buffer);
-		
+	char *hold=buffer;
 
-		
-	}*/
-	int num = 1;
+	int i = 0;
 	fgets(buffer,1000,stdin);
-	struct movie *a = malloc(sizeof(movie));
-	a->color=strsep(&buffer,",");
-	a->director_name=strsep(&buffer,",");
 	
-	a->num_critic_for_reviews=atoi(strsep(&buffer,","));
-	a->duration=atoi(strsep(&buffer,","));
+	struct movie* array = (movie*)malloc(sizeof(struct movie) * 5048);
 	
-	a->director_facebook_likes=atoi(strsep(&buffer,","));
-	a->actor_3_facebook_likes=atoi(strsep(&buffer,","));
-	a->actor_2_name=strsep(&buffer,",");
-	a->actor_1_facebook_likes=atoi(strsep(&buffer,","));
-	a->gross=atoi(strsep(&buffer,","));
-	a->genres=strsep(&buffer,",");
-	a->actor_1_name=strsep(&buffer,",");
-	a->movie_title=strsep(&buffer,"Â ");
-	if(a->movie_title[0] =='\"')
-	{a->movie_title=a->movie_title+1;}
-	strsep(&buffer,",");
+	while(fgets(buffer, 1000, stdin)){
+		
+		printf("hi\n");
+		
+		
+			//struct movie *a = malloc(sizeof(movie));
+			//array[i] = *a;
+			array[i].color=strsep(&buffer,",");
+			array[i].director_name=strsep(&buffer,",");
+		
+			char* token = strsep(&buffer,",");
+			
+		if(token[0] == '\0'){
+			array[i].num_critic_for_reviews = -1;
+		} else {
+			array[i].num_critic_for_reviews = atoi(token);
+		}
+		
+		array[i].duration=atoi(strsep(&buffer,","));
+		
+		array[i].director_facebook_likes=atoi(strsep(&buffer,","));
+		array[i].actor_3_facebook_likes=atoi(strsep(&buffer,","));
+		array[i].actor_2_name=strsep(&buffer,",");
+		array[i].actor_1_facebook_likes=atoi(strsep(&buffer,","));
+		array[i].gross=atoi(strsep(&buffer,","));
+		array[i].genres=strsep(&buffer,",");
+		array[i].actor_1_name=strsep(&buffer,",");
+
+
+		array[i].movie_title=strsep(&buffer, "Â ");
+			
+
+		if(array[i].movie_title[0] =='\"'){
+			array[i].movie_title = array[i].movie_title+1;
+		}
+		
+		strsep(&buffer,",");
+		
+		array[i].num_voted_users=atoi(strsep(&buffer,","));
+		array[i].cast_total_facebook_likes=atoi(strsep(&buffer,","));
+		array[i].actor_3_name=strsep(&buffer,",");
+		array[i].facenumber_in_poster=atoi(strsep(&buffer,","));
+		array[i].plot_keywords=strsep(&buffer,",");
+		array[i].movie_imdb_link=strsep(&buffer,",");
+		array[i].num_user_for_reviews=atoi(strsep(&buffer,","));
+		array[i].language=strsep(&buffer,",");
+		array[i].country=strsep(&buffer,",");
+		array[i].content_rating=strsep(&buffer,",");
+		array[i].budget=atoi(strsep(&buffer,","));
+		array[i].title_year=atoi(strsep(&buffer,","));
+		array[i].actor_2_facebook_likes=atoi(strsep(&buffer,","));
+		array[i].imdb_score_num=atoi(strsep(&buffer,","));
+		array[i].aspect_ratio=atoi(strsep(&buffer,","));
+		array[i].movie_facebook_likes=atoi(strsep(&buffer,","));
+		
+		
+		printf("%s\n", array[i].movie_title);
+		printf("%i\n", array[i].num_critic_for_reviews);
+		printf("%s\n", array[i].country);
+		
+		//array[i] = *a;
+		
+		i++;
+		
+		
+		printf("Movie No: %i\n",i);
+		buffer = hold;
+	}
 	
-	a->num_voted_users=atoi(strsep(&buffer,","));
-	a->cast_total_facebook_likes=atoi(strsep(&buffer,","));
-	a->actor_3_name=strsep(&buffer,",");
-	a->facenumber_in_poster=atoi(strsep(&buffer,","));
-	a->plot_keywords=strsep(&buffer,",");
-	a->movie_imdb_link=strsep(&buffer,",");
-	a->num_user_for_reviews=atoi(strsep(&buffer,","));
-	a->language=strsep(&buffer,",");
-	a->country=strsep(&buffer,",");
-	a->content_rating=strsep(&buffer,",");
-	a->budget=atoi(strsep(&buffer,","));
-	a->title_year=atoi(strsep(&buffer,","));
-	a->actor_2_facebook_likes=atoi(strsep(&buffer,","));
-	a->imdb_score_num=atoi(strsep(&buffer,","));
-	a->aspect_ratio=atoi(strsep(&buffer,","));
-	a->movie_facebook_likes=atoi(strsep(&buffer,","));
+	printf("%s\n",array[5000].movie_title);
 	
 	
-	printf("%s\n",a->movie_title);
-	printf("%i\n",a->num_voted_users);
+	//int index = 0;
+	
+	/*for(index = 0; index < 2048; index++){
+		free(&array[index]);
+	}*/
+	
+	
+	free(array);
 	
 	return 0;
 }
