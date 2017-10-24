7268084ca0830824e134ee9288ca42cf00882da2
Maxwell E. Mucha <mem434@factory.cs.rutgers.edu>
2017-10-24 18:30:14 -0400

    Without repeats, searches through default directory and every subdirectory for every csv file, and sorts them. Usage "./sorter -c FIELD" TODO: do input checks, allow for path specifications

--- MultiProcess Sorter/sorter.c
+++ MultiProcess Sorter/sorter.c
@@ -13,16 +13,131 @@
 #include <ctype.h>
 #include "mergesort.c"
 #include "Sorter.h"
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+int numchild;
+char*sortname;	//Name of the file being sorted
+char*sortpath;	//Path leading to the file being sorted
+char*fullname;
+char*category;
+int main(int argc, char **argv)
+{
 
-int main(int argc, char **argv) {
+	char path[4096];
+	// gets cwd, you'll need to change this so that it give you the correct starting directory with different inputs
+	if(getcwd(path,sizeof(path))!=NULL) 
+	{
+		//printf("%s\n",path);
+	}
+	else
+	{
+		printf("Error with getcwd");
+		return -1;
+	}
+	numchild=0; 	// Number of children a process spawns
+	sortpath=path;
+	//Directory and directory structure
+	DIR *dir;
+    struct dirent *str;
+    dir = opendir(path);
+    
+    
+    if ( dir != NULL) // null check for directory
+    {
+		str = readdir(dir); // reads the first thing in the directory
+		for(;str != NULL;str = readdir(dir)) //for loop that goes through everything in the loop.
+		{
+			if((strcmp(str->d_name,"..") != 0) && (strcmp(str->d_name,".") != 0)) //checks for .  and .. directories
+			{
+				if(str->d_type == DT_DIR) // if its a directory
+				{
+					int pid = fork(); // fork here
+					if(pid == 0) //if its a child
+					{
+						closedir(dir); //close the original directory
+						strcat(path,"/"); //append the folder to the path
+						strcat(path, str->d_name);
+						//printf("%s\n",nextdir);
+						dir = opendir(path);	//open the new directory
+						numchild = 0;	//reset number of children			
+					}
+					else
+					{
+						numchild++; //increment number of children in parent
+					}
+					
+				}
+				else
+				{
+					char* extension = strrchr(str->d_name,'.'); // gets the extension of the file
+					if(extension!=NULL)
+					{
+						if(strcmp(extension,".csv")==0)			// ALL CSV FILES GO HERE
+						{
+							int pid = fork(); // fork on here
+							if(pid == 0) // if its  a child
+							{
+								numchild = 0; //reset number of children
+								sortname = str->d_name; //set the name of the file
+								sortpath = path; //set the path of the file
+								break; //exit look
+							}
+							else
+							{		
+								numchild++; //increment number of children in parent
+							}			
+						}
+					}
+					
+				}		
+			}	
+		}	
+	}
+	closedir(dir); //close whatever directory is open.
 	
+	//at this point every file and every directory has its own process
+
+	int i;
+	if(sortpath != NULL && sortname == NULL) // goes in here if its a directory fork.
+	{
+			for(i = numchild;i>0;i--) // wait for all the children that the directory spawned. 
+									  //returns if a directory has no children, or finishes waiting
+			{
+				//printf("[%i] waiting on child #%i\n", getpid(),i);
+				wait();
+				//printf("[%i] wait [%i] complete\n",getpid(),i);
+			}
+		return 0;
+	}	
+	
+	//printf("[%i] %s/%s\n",getpid(),sortpath,sortname);
+	fullname = malloc(4096); //for some reason sortpath and sortname get corrupted
+																//so if you want to use them for something else
+																//you'll need to malloc them into a variable.
+	fullname = strcpy(fullname,sortpath);
+	fullname = strcat(fullname,"/");
+	fullname = strcat(fullname,sortname); // Sets the full path name 
+	
+	
+	
+	printf("[%i] %s\n",getpid(),fullname);
 	char* buffer = malloc(1000);
 	char* hold = buffer;
 	int size = 128;
-	int i = 0;
+	i = 0;
+	FILE* fp = fopen(fullname,"r");
 	
-	fgets(buffer, 1000, stdin);
+	fgets(buffer, 1000, fp);
+
+
+	//You'll need to edit the stuff below here to make sure the input path is the correct one.
+	//Also you may need to fix the input error checking.
+	//If you want to output to a different path you'll need to do something in printcsv
+	//to write to the right output
+
 
 	if (argv[1][0] != '-' || argv[1][1] != 'c'){ //argument 1
 		printf("ERROR: Expected '-c' as first argument.\n");
@@ -40,7 +155,7 @@ int main(int argc, char **argv) {
 	while(token != NULL){
 		//printf("%s\n",token);
 		if(strcmp(token, argv[2]) == 0 || strcmp(argv[2], "movie_facebook_likes")==0){	
-			printf("%s\n",token);
+			//printf("%s\n",token);
 			isin = 1;
 			break;
 		}
@@ -57,11 +172,12 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 	
-	char* category = argv[2]; //category/column that needs to be sorted
+	
+	category = argv[2]; //category/column that needs to be sorted
 	
 	struct movie* array = malloc(sizeof(struct movie) * size);
-	
-	while(fgets(buffer, 1000, stdin)){
+	//printf("[%i] FILEPATH IS STILL\n %s/%s\n",getpid(),sortpath,sortname);
+	while(fgets(buffer, 1000, fp)){
 		
 		//Array size increaser
 		if (i == size-1){
@@ -315,9 +431,8 @@ int main(int argc, char **argv) {
 	
 	int m = i;
 	int n = 27;
-	
+	//printf("[%i] FILEPATH IS STILL\n %s/%s\n",getpid(),sortpath,sortname);
 	mergesort(array, category, 0, (m - 1)); //MERGE SORT
-	
 	printCSV(array, m, n); //PRINT OUTPUT/SORTED ARRAY
 	
 	// Free malloc'd fields/variables
@@ -337,7 +452,7 @@ int main(int argc, char **argv) {
 		free(array[index].country);
 		free(array[index].content_rating);
 	}
-	
+	free(fullname);
 	free(array);
 	free(buffer);
 	
@@ -373,34 +488,41 @@ void trim(char* token) {
 	}
 	
 } //end of 'trim' function
-
+//fullname
 void printCSV(struct movie* array, int m, int n) {
 	
-	//FILE *fp;
+	FILE *fp2;
 	int i;
-	//char name[] = "sortedmovies";
+	//you wont be able to use
+	
+	
+	char*extension = strrchr(fullname,'.'); //properly formats the name of the output file
+	strcpy(extension,"-sorted-");
+	strcat(extension,category);
+	strcat(extension,".csv");
+	//printf("%s\n",fullname);
 	//char* filename = name;
 	//filename = strcat(name, ".csv");
-	//fp = fopen(filename, "w+");
+	fp2 = fopen(fullname, "w+");
 	
 	// Print column titles
-	//fprintf(fp, "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes, actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes\n");
-	fprintf(stdout, "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes, actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes\n");
+	fprintf(fp2, "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes, actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes\n");
+	//fprintf(stdout, "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes, actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes\n");
 	
 	// Print each struct (each movie is 1 row with 28 columns)
 	for(i = 0; i < m; i++){
 		
-		if(strchr(array[i].movie_title, ',') || strchr(array[i].movie_title, ';')){
-			//fprintf(fp, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,\"%s\",%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);	
-			fprintf(stdout, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,\"%s\",%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);		
+		if(strchr(array[i].movie_title, ',')){
+			fprintf(fp2, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,\"%s\",%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);	
+			//fprintf(stdout, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,\"%s\",%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);		
 		} else {
-			//fprintf(fp, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,%s,%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);
-			fprintf(stdout, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,%s,%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);
+			fprintf(fp2, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,%s,%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);
+			//fprintf(stdout, "%s,%s,%i,%i,%i,%i,%s,%i,%i,%s,%s,%s,%i,%i,%s,%i,%s,%s,%i,%s,%s,%s,%i,%i,%i,%f,%f,%i\n", array[i].color,array[i].director_name,array[i].num_critic_for_reviews,array[i].duration,array[i].director_facebook_likes,array[i].actor_3_facebook_likes,array[i].actor_2_name,array[i].actor_1_facebook_likes,array[i].gross,array[i].genres,array[i].actor_1_name,array[i].movie_title,array[i].num_voted_users,array[i].cast_total_facebook_likes,array[i].actor_3_name,array[i].facenumber_in_poster,array[i].plot_keywords,array[i].movie_imdb_link,array[i].num_user_for_reviews,array[i].language,array[i].country,array[i].content_rating,array[i].budget,array[i].title_year,array[i].actor_2_facebook_likes,array[i].imdb_score_num,array[i].aspect_ratio,array[i].movie_facebook_likes);
 		}
 		
 		
 	}
-	
+	//printf("[%i] FILEPATH IS STILL\n %s/%s\n",getpid(),sortpath,sortname);
 	//fclose(fp);
 	
 	
